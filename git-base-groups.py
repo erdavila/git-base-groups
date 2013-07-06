6527565df306ab77c26222a1718dee0d4be91fda
Eduardo R. D'Avila <erdavila@gmail.com>
2013-07-06 16:01:30 -0300

    Use Group object

--- git-base-groups.py
+++ git-base-groups.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 from __future__ import print_function
+from collections import namedtuple
 import subprocess
 '''
 	TODO:
 		list merge-bases in each group
 		filter (tags? branches? remotes? all?)
 '''
+
+
+Group = namedtuple('Group', ['base', 'refs'])
+
 
 def main():
 	groups = []
@@ -15,22 +20,21 @@ def main():
 	for ref in refs:
 		added = False
 		for group in groups:
-			group_first_ref = group[0]
 			try:
 				with open('/dev/null', 'w') as null:
-					subprocess.check_call(['git', 'merge-base', ref, group_first_ref], stdout=null)
+					subprocess.check_call(['git', 'merge-base', ref, group.base], stdout=null)
 			except subprocess.CalledProcessError:
 				pass
 			else:
-				group.append(ref)
+				group.refs.append(ref)
 				added = True
 				break
 		if not added:
-			new_group = [ref]
+			new_group = Group(base=ref, refs=[ref])
 			groups.append(new_group)
 	
 	for group in groups:
-		for ref in group:
+		for ref in group.refs:
 			print(ref)
 		
 		print()
